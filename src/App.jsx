1527998e750c563368cdb77264cdfa6530f31ce5
Omar Saad <saadomar343@gmail.com>
2024-09-14 15:12:59 +0300

    finished the page added: - join us - supporters - footer

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,18 @@ import hyprland from "/svgs/hyprland.svg";
 import cachyos from "/svgs/cachyOS.svg";
 import ublue from "/ublue.png";
 import terminal from "/svgs/terminal.svg";
+import discord from "/svgs/discord.svg";
+import github from "/svgs/github.svg";
 
-const svgsClassName = "absolute -bottom-5 -left-5 rotate-[-20deg] h-24 w-24 grayscale"
+import aarch from "/sponsor/aarch.png";
+import arco from "/sponsor/arco-linux.png";
+import foss from "/sponsor/foss-host.png";
+
+import githubUsers from "/github-users.png";
+import discordServer from "/discord-server.png";
+
+const svgsClassName =
+  "absolute -bottom-5 -left-5 rotate-[-20deg] h-24 w-24 grayscale";
 
 function App() {
   const infoCards = [
@@ -58,54 +68,151 @@ function App() {
   const featuresCards = [
     {
       title: "Hyprland",
-      description: "Axyl focuses fully on tiling WMs, so we can give you the best experience for advanced users.",
+      description:
+        "Axyl focuses fully on tiling WMs, so we can give you the best experience for advanced users.",
       bg: "#2E3440",
-      children: <img className={svgsClassName} alt="hyprland" src={hyprland} />
+      children: <img className={svgsClassName} alt="hyprland" src={hyprland} />,
     },
     {
       title: "Arch Linux tweak tool",
-      description: "A versatile and powerful tool for customizing Arch-based systems with ease!",
+      description:
+        "A versatile and powerful tool for customizing Arch-based systems with ease!",
       bg: "#1A1B26",
-      children: <img className={svgsClassName} alt="archlinux" src={arch} />
+      children: <img className={svgsClassName} alt="archlinux" src={arch} />,
     },
     {
       title: "CachyOS-Kernel",
-      description: "CachyOS-Kernal is a lightweight and modular kernel for Arch based systems.",
+      description:
+        "CachyOS-Kernal is a lightweight and modular kernel for Arch based systems.",
       bg: "#1E222A",
-      children: <img className={svgsClassName} alt="CachyOS-Kernel" src={cachyos} />
+      children: (
+        <img className={svgsClassName} alt="CachyOS-Kernel" src={cachyos} />
+      ),
     },
     {
       title: "Yafti",
-      description: "Responsible for installing Flatpaks on first boot after a user finishes installation. It is intended as a replacement for custom zenity dialogs.",
+      description:
+        "Responsible for installing Flatpaks on first boot after a user finishes installation. It is intended as a replacement for custom zenity dialogs.",
       bg: "#222526",
-      children: <img className={svgsClassName} alt="Yafti" src={ublue} />
+      children: <img className={svgsClassName} alt="Yafti" src={ublue} />,
     },
     {
       title: "AGS",
-      description: "AGS allows defining GTK widgets in declarative way, It also provides services and other utilities to interact with the system to make widgets more functional and user-friendly.",
+      description:
+        "AGS allows defining GTK widgets in declarative way, It also provides services and other utilities to interact with the system to make widgets more functional and user-friendly.",
       bg: "#1A1B26",
-      children: <img className={svgsClassName} alt="AGS" src={terminal} />
-    }
-  ]
-  
+      children: <img className={svgsClassName} alt="AGS" src={terminal} />,
+    },
+  ];
+
   return (
     <div className="">
       <Navbar />
       <Hero />
+
+      {/* Thesis */}
       <Section title="What is Axyl?">
         {infoCards.map(({ title, description, bg, children }) => (
-          <Card key={title} title={title} description={description} bg={bg || "transparent"}>
+          <Card
+            key={title}
+            title={title}
+            description={description}
+            bg={bg || "transparent"}
+          >
             {children}
           </Card>
         ))}
       </Section>
-      <Section title="Features" description="Axyl provides a lot of features that you can use and save much time and work.">
+
+      {/* Features */}
+      <Section
+        title="Features"
+        description="Axyl provides a lot of features that you can use and save much time and work."
+      >
         {featuresCards.map(({ title, description, bg, children }) => (
-          <Card key={title} title={title} description={description} bg={bg || "transparent"}>
-            {children}  
+          <Card
+            key={title}
+            title={title}
+            description={description}
+            bg={bg || "transparent"}
+          >
+            {children}
           </Card>
         ))}
       </Section>
+
+      {/* Join us */}
+      <Section
+        title="Join Us!"
+        classNames={{
+          wrapper: "!grid-cols-1 !p-9",
+          header: "lg:!text-start lg:!items-start",
+        }}
+      >
+        <div className="flex w-full gap-6">
+          <Card
+            title="GitHub"
+            description="A tiling window manager that represents a full binary tree."
+          >
+            <a
+              href={import.meta.env.VITE_GITHUB}
+              className="button-primary h-fit px-8 py-4"
+            >
+              Check out our repo
+            </a>
+            <img
+              className="absolute -bottom-4 -left-8 -z-10 w-full max-w-64 self-center px-8 py-4"
+              alt="GitHub Logo"
+              src={github}
+            />
+            <img
+              alt="GitHub users"
+              className="absolute -bottom-4 -right-8 -z-10 w-full max-w-64 self-center grayscale"
+              src={githubUsers}
+            />
+          </Card>
+          <Card
+            title="Discord"
+            description="Axyl focuses fully on tiling WMs, so we can give you the best experience for advanced users."
+          >
+            <a
+              href={import.meta.env.VITE_DISCORD}
+              className="button-primary h-fit px-8 py-4"
+            >
+              Discord community
+            </a>
+            <img
+              className="absolute -bottom-4 -left-8 -z-10 w-full max-w-48 self-center"
+              alt="Discord Logo"
+              src={discord}
+            />
+            <img
+              alt="Discord Server"
+              className="absolute -bottom-24 -right-24 -z-10 w-full max-w-48 rotate-[30deg] self-center grayscale"
+              src={discordServer}
+            />
+          </Card>
+        </div>
+      </Section>
+
+      <section className="flex flex-col items-center gap-6 border-b border-t border-[var(--border)] p-9">
+        <h1 className="text-2xl font-bold text-[var(--text)]">
+          Our Supporters
+        </h1>
+        <div className="flex w-full gap-9 justify-center">
+          <img src={arco} alt="arco" className="grayscale hover:grayscale-0 transition duration-700 h-24 w-fit" />
+          <img src={foss} alt="foss" className="grayscale hover:grayscale-0 transition duration-700 h-24 w-fit" />
+          <img src={aarch} alt="aarch" className="grayscale hover:grayscale-0 transition duration-700 h-24 w-fit" />
+        </div>
+      </section>
+
+      <section className="p-9 border-t mb-64 border-b border-[var(--border)] items-center justify-center flex flex-col gap-9">
+        <div>
+        <h1 className="text-2xl font-bold text-[var(--text)]">Want to boost your workflow?</h1>
+        <p className="text-center text-pretty text-[var(--light-text)] max-w-md">Join the thousand people who have downloaded Axyl. Try it now in a live ISO - no need to install.</p>
+        </div>
+        <button className="button-primary h-fit px-8 py-4">Download Now</button>
+      </section>
     </div>
   );
 }
--- src/components/section.jsx
+++ src/components/section.jsx
@@ -1,8 +1,8 @@
-export default function Section({ children, title, description }) {
+export default function Section({ children, title, description, classNames = {wrapper: "", header: ""} }) {
   return (
-    <section className="grid w-full grid-flow-row gap-6 lg:px-[72px] px-9 py-10 lg:grid-cols-2 lg:py-16">
-      <div className="flex w-full flex-col gap-2.5 px-9 py-8 text-start lg:items-end lg:text-end">
-        <h1 className="text-[32px] font-bold leading-8 text-[var(--text)]">
+    <section className={`${classNames.wrapper} grid w-full grid-flow-row gap-6 lg:px-[72px] px-9 py-10 lg:grid-cols-2 lg:py-16`}>
+      <div className={`${classNames.header} flex w-full flex-col gap-2.5 px-9 py-8 text-start lg:items-end lg:text-end`}>
+        <h1 className={`text-[32px] font-bold leading-8 text-[var(--text)]`}>
           {title}
         </h1>
         <p className="max-w-sm text-pretty text-[var(--light-text)]">
